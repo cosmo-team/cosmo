23570b13cde71e4a873f38e29bc7a6922d7df16f
alexbowe <bowe.alexander@gmail.com>
2014-04-30 16:56:39 +0900

    Reading and printing hex representations from buffer.

--- convert_dsk.c
+++ convert_dsk.c
@@ -3,13 +3,44 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
 #include "debug.h"
 //#include "nanotime.h"
 
 const char * USAGE = "<DSK output file>";
-
+const size_t MAX_BITS_PER_KMER = 128;
 const size_t BUFFER_SIZE = 0x8000; // 32Kb data buffer
+
+int dsk_read_header(int, uint32_t *, uint32_t *);
+uint64_t swap_gt_64(uint64_t);
+int compare_64(const void *, const void *);
+int compare_128(const void *, const void *);
+
+inline uint64_t swap_gt_64(uint64_t x) {
+  return x ^ ((x & 0xAAAAAAAAAAAAAAAA) >> 1);
+}
+
+int compare_64(const void * lhs, const void * rhs)
+{
+  uint64_t a = *(uint64_t*)lhs;
+  uint64_t b = *(uint64_t*)rhs;
+  return a - b;
+}
+
+int compare_128(const void * lhs, const void * rhs) {
+  uint64_t a_upper = ((uint64_t*)lhs)[0];
+  uint64_t a_lower = ((uint64_t*)lhs)[1];
+  uint64_t b_upper = ((uint64_t*)rhs)[0];
+  uint64_t b_lower = ((uint64_t*)rhs)[1];
+  return (a_upper - b_upper) + (a_lower - b_lower);
+}
+
+int dsk_read_header(int handle, uint32_t * kmer_num_bits, uint32_t * k) {
+  int success = read(handle, (char*)kmer_num_bits, sizeof(uint32_t)) != -1 &&
+                read(handle, (char*)k, sizeof(uint32_t)) != -1;
+  return success;
+}
 
 int main(int argc, char * argv[]) {
   // parse argv file
@@ -29,17 +60,23 @@ int main(int argc, char * argv[]) {
   // read header
   uint32_t kmer_num_bits = 0;
   uint32_t k = 0;
-  if ( (read(handle, (char*)&kmer_num_bits, sizeof(uint32_t))) == -1 ||
-       (read(handle, (char*)&k, sizeof(uint32_t))) == -1 ) {
+  if ( !dsk_read_header(handle, &kmer_num_bits, &k) )
+  {
     fprintf(stderr, "Error reading file %s\n", file_name);
     exit(1);
   }
-  TRACE("kmer_num_bits, k = %d, %d\n", kmer_num_bits, k);
   uint32_t kmer_num_blocks = (kmer_num_bits / 8) / sizeof(uint64_t);
+  TRACE("kmer_num_bits, k = %d, %d\n", kmer_num_bits, k);
   TRACE("kmer_num_blocks = %d\n", kmer_num_blocks);
 
+  if (kmer_num_bits > MAX_BITS_PER_KMER) {
+    fprintf(stderr, "Kmers larger than %zu bits are not currently supported. %s uses %d bits per kmer.\n",
+        MAX_BITS_PER_KMER, file_name, kmer_num_bits);
+    exit(1);
+  }
+
   // read how many items there are
-  size_t record_size = (kmer_num_bits / 8) + 4; // record fmt: kmer, count
+  size_t record_size = (kmer_num_bits / 8) + sizeof(uint32_t); // record fmt: kmer, count
   off_t original_pos = -1;
   off_t end_pos = -1;
   if ( (original_pos = lseek(handle, 0 ,SEEK_CUR)) == -1 ||
@@ -54,45 +91,87 @@ int main(int argc, char * argv[]) {
   size_t num_records = (end_pos - original_pos)/record_size;
   TRACE("num_records = %zu\n", num_records);
 
-  // allocate an array for input
-  if (kmer_num_bits > 64) {
-    uint64_t b[2];
-    read(handle, (char*)&b, sizeof(uint64_t) * 2);
-    TRACE("%016llx%016llx\n", b[0], b[1]);
-    fprintf(stderr, "Not yet implemented for k > 32\n");
-    exit(1);
-  }
   typedef uint64_t kmer_t[kmer_num_blocks];
   kmer_t * kmers = calloc(num_records, sizeof(kmer_t));
+  size_t next_slot = 0;
 
   // read the items items into the array via a buffer
   char input_buffer[BUFFER_SIZE];
   memset(input_buffer, 0, BUFFER_SIZE);
   // Only read a multiple of records... we are going to remove the counts anyway
-  size_t read_size = BUFFER_SIZE / record_size;
+  // So this makes it easier to iterate over
+  size_t read_size = (BUFFER_SIZE / record_size) * record_size;
   ssize_t num_bytes_read = 0;
   do {
     if ( (num_bytes_read = read(handle, input_buffer, read_size)) == -1 ) {
       fprintf(stderr, "Error reading file %s\n", argv[1]);
       exit(1);
     }
-    // TODO: iterate over, adding the kmers, as curr_byte < num_bytes_read...
-    // TODO: make function that fills the buffer 
-    // curr_byte += sizeof(uint64_t) * 2
-    // curr_byte += sizeof(uint32_t)
     if (num_bytes_read ) { 
-      TRACE("%016llx\n", (uint64_t)((uint64_t*)input_buffer)[0]);
+      ssize_t offset = 0;
+      TRACE("num_bytes_read = %zd\n", num_bytes_read);
+      while (offset < num_bytes_read) {
+        kmer_t * kmer = (kmer_t*)(input_buffer+offset);
+        if (kmer_num_blocks == 1) {
+          kmers[next_slot][0] = *kmer[0];
+          //printf("%016llx\n", *kmer[0]);
+        }
+        else if (kmer_num_blocks == 2)
+        {
+          // TODO: move this if statement outside the loop
+          kmers[next_slot][0] = ((uint64_t*)(input_buffer + offset))[0];
+          kmers[next_slot][1] = ((uint64_t*)(input_buffer + offset))[1];
+          //printf("%016llx%016llx\n", upper, lower);
+        }
+        offset += sizeof(kmer_t) + sizeof(uint32_t);
+        next_slot++;
+      }
     }
   } while ( num_bytes_read );
 
   close(handle);
 
-  // TODO: version control
-  // refactor
+  assert(next_slot == num_records);
+  TRACE("next_slot = %zu\n", next_slot);
+
+  for (size_t i = 0; i < num_records; i++)
+  {
+    if (kmer_num_blocks == 1) {
+      printf("%016llx\n", *kmers[i]);
+    }
+    else if (kmer_num_blocks == 2) {
+      uint64_t upper = kmers[i][0];
+      uint64_t lower = kmers[i][1];
+      printf("%016llx %016llx\n", lower, upper);
+    }
+  }
+  /*
+  printf("SORTING...\n");
+  if (kmer_num_blocks == 1) {
+    qsort(kmers, num_records, sizeof(uint64_t), compare_64);
+  }
+  else if (kmer_num_blocks == 2) {
+    qsort(kmers, num_records, 2 * sizeof(uint64_t), compare_128);
+  }
+
+  for (size_t i = 0; i < num_records; i++)
+  {
+    if (kmer_num_blocks == 1) {
+      printf("%016llx\n", *kmers[i]);
+    }
+    else if (kmer_num_blocks == 2) {
+      uint64_t upper = kmers[i][0];
+      uint64_t lower = kmers[i][1];
+      printf("%016llx %016llx\n", lower, upper);
+    }
+  }
+  */
+  // TODO: refactor
+  //
+  // Convert to normal format
   // change buffer size to 2x
   // iterate over and read them into buffer
-  // + their reverse complements
-  // print them out to make sure its being read
+  // add their reverse complements
   // Convert
   // Sort
   // Filter dummy edges
