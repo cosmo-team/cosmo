d52ff10073299ef6635451bf3da342dd5d654e4a
Alex Bowe <alex@megami.local>
2014-05-14 13:07:50 +0900

    Added reverse-only lookup table

--- make_lut.py
+++ make_lut.py
@@ -5,7 +5,12 @@ doc = """
 #ifndef LUT_H
 #define LUT_H
 
+#define reverse_8(x) (reverse_8_lut[(x)])
 #define revcomp_8(x) (revcomp_8_lut[(x)])
+
+unsigned char reverse_8_lut[256] = {
+  %s
+};
 
 unsigned char revcomp_8_lut[256] = {
   %s
@@ -16,7 +21,9 @@ unsigned char revcomp_8_lut[256] = {
 reverse_8 = lambda x: ((x & 0xC0) >> 6) | (x << 6) | ((x & 0x30) >> 2) | ((x & 0x0C) << 2)
 complement = lambda x: ~x
 
-bytes     = np.array(range(256), np.uint8)
-rev_comps = complement(reverse_8(bytes))
+all_bytes = np.array(range(256), np.uint8)
+rev_bytes = reverse_8(all_bytes)
+rev_comps = complement(rev_bytes)
 
-print doc % (",\n  ".join(map(hex,rev_comps.tolist())))
+print doc % (",\n ".join(map(hex, rev_bytes.tolist())),
+             ",\n  ".join(map(hex,rev_comps.tolist())))
