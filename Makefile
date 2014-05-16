c8577b2878055432273f07ba4a4d237955f7de62
alexbowe <bowe.alexander@gmail.com>
2014-05-16 16:44:03 +0900

    Moved IO code into separate compile unit.

--- Makefile
+++ Makefile
@@ -4,17 +4,20 @@ C_FLAGS=-m64 -std=c99 -pedantic -W -Wall -Wextra -Wshadow -Wpointer-arith -Wcast
 DEBUG_FLAGS=-g
 RELEASE_FLAGS=-O3 -DNDEBUG
 EXPERIMENT_FLAGS=-DEXPERIMENT
-COMPILE=$(CC) -o convert_dsk convert_dsk.c $(C_FLAGS)
+COMPILE=$(CC) -o convert_dsk convert_dsk.c io.o $(C_FLAGS)
 
 default: debug
 
 lut.h: make_lut.py
 		python make_lut.py > lut.h
 
-debug: convert_dsk.c lut.h debug.h nanotime.h
+io.o: io.h io.c
+		$(CC) $(C_FLAGS) $(DEBUG_FLAGS) -c io.c
+
+debug: convert_dsk.c lut.h debug.h nanotime.h io.o
 		$(COMPILE) $(DEBUG_FLAGS)
 
-release: convert_dsk.c lut.h debug.h nanotime.h
+release: convert_dsk.c lut.h debug.h nanotime.h io.o
 		$(COMPILE) $(RELEASE_FLAGS)
 
 clean:
--- convert_dsk.c
+++ convert_dsk.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <limits.h>
-
+#include "common.h"
+#include "io.h"
 #include "lut.h"
-#include "debug.h"
 #include "uint128_t.h"
 #include "nanotime.h"
 
 #define CPU // CUDA?
 
 const char * USAGE = "<DSK output file>";
-const size_t MAX_BITS_PER_KMER = 128;
-const size_t BUFFER_SIZE = 0x8000; // 32Kb data buffer
 
-int dsk_read_header(int, uint32_t *, uint32_t *);
 uint64_t swap_gt_64(uint64_t);
 int compare_64(const void *, const void *);
 int compare_128(const void *, const void *);
@@ -124,18 +115,6 @@ int compare_128(const void * lhs, const void * rhs) {
   return (a_upper - b_upper) + (a_lower - b_lower);
 }
 
-int dsk_read_header(int handle, uint32_t * kmer_num_bits, uint32_t * k) {
-  int success = read(handle, (char*)kmer_num_bits, sizeof(uint32_t)) != -1 &&
-                read(handle, (char*)k, sizeof(uint32_t)) != -1;
-  return success;
-}
-
-size_t dsk_record_size(uint32_t);
-inline size_t dsk_record_size(uint32_t kmer_num_bits) {
-  // record fmt: kmer, count
-  return (kmer_num_bits/8) + 4;
-}
-
 void print_kmers_hex(FILE * outfile, uint64_t * kmers, size_t num_kmers, uint32_t kmer_num_bits);
 void print_kmers_hex(FILE * outfile, uint64_t * kmers, size_t num_kmers, uint32_t kmer_num_bits) {
   assert(kmer_num_bits <= 128);
@@ -164,84 +143,6 @@ void print_kmers_dec(FILE * outfile, uint64_t * kmers, size_t num_kmers, uint32_
       fprintf(outfile, "%020llu %020llu\n", upper, lower);
     }
   }
-}
-
-int dsk_num_records(int handle, uint32_t kmer_num_bits, size_t * num_records);
-int dsk_num_records(int handle, uint32_t kmer_num_bits, size_t * num_records) {
-  size_t record_size = dsk_record_size(kmer_num_bits);
-  off_t original_pos = -1;
-  off_t end_pos = -1;
-  if ( (original_pos = lseek(handle, 0 ,SEEK_CUR)) == -1 ||
-       (end_pos = lseek(handle, 0, SEEK_END)) == -1  ) {
-    return -1;
-  }
-  if ( lseek(handle, original_pos, SEEK_SET) == -1 ) {
-    return -1;
-  }
-  *num_records = (end_pos - original_pos)/record_size;
-  return 0;
-}
-
-size_t dsk_read_kmers(int handle, uint32_t kmer_num_bits, uint64_t * kmers_output);
-size_t dsk_read_kmers(int handle, uint32_t kmer_num_bits, uint64_t * kmers_output) {
-  // TODO: Add a parameter to specify a limit to how many records we read (eventually multipass merge-sort?)
-
-  // read the items items into the array via a buffer
-  char input_buffer[BUFFER_SIZE];
-
-  // Only read a multiple of records... this makes it easier to iterate over
-  size_t record_size = dsk_record_size(kmer_num_bits);
-  size_t read_size = (BUFFER_SIZE / record_size) * record_size;
-
-  // Technically we *could* read in bytes at a time if need-be... but nah.
-  assert(read_size > 0);
-
-  ssize_t num_bytes_read = 0;
-  size_t next_slot = 0;
-
-  // This if statement would be more readable inside the loop, but it's moved out here for performance.
-  // TODO: This might run better if we filled in another buffer on the stack with kmers only, then memcpy to the output?
-  if (kmer_num_bits <= 64) {
-    do {
-      // Try read a batch of records.
-      if ( (num_bytes_read = read(handle, input_buffer, read_size)) == -1 ) {
-        return -1;
-      }
-
-      // Did we read anything?
-      if (num_bytes_read ) {
-        TRACE("num_bytes_read = %zd\n", num_bytes_read);
-
-        // Iterate over kmers, skipping counts
-        for (ssize_t offset = 0; offset < num_bytes_read; offset += sizeof(uint64_t) + sizeof(uint32_t), next_slot += 1) {
-          kmers_output[next_slot] = swap_gt_64(*((uint64_t*)(input_buffer + offset)));
-        }
-      }
-    } while ( num_bytes_read );
-  }
-  else if (64 < kmer_num_bits <= 128) {
-    do {
-      // Try read a batch of records.
-      if ( (num_bytes_read = read(handle, input_buffer, read_size)) == -1 ) {
-        return -1;
-      }
-
-      // Did we read anything?
-      if (num_bytes_read ) { 
-        TRACE("num_bytes_read = %zd\n", num_bytes_read);
-
-        // Iterate over kmers, skipping counts
-        for (ssize_t offset = 0; offset < num_bytes_read; offset += 2 * sizeof(uint64_t) + sizeof(uint32_t), next_slot += 2) {
-            // Swapping lower and upper block (to simplify sorting later)
-            kmers_output[next_slot + 1] = swap_gt_64(*((uint64_t*)(input_buffer + offset)));
-            kmers_output[next_slot]     = swap_gt_64(*((uint64_t*)(input_buffer + offset + sizeof(uint64_t))));
-        }
-      }
-    } while ( num_bytes_read );
-  }
-  else assert (kmer_num_bits <= 128);
-  // Return the number of kmers read (whether 64 bit or 128 bit)
-  return next_slot / ((kmer_num_bits/8)/sizeof(uint64_t));
 }
 
 int main(int argc, char * argv[]) {
@@ -325,8 +226,8 @@ int main(int argc, char * argv[]) {
   }
   #endif
 
-  kmer_t * reverse_complements = kmers + num_records;
-  add_reverse_complements(kmers, reverse_complements, num_records);
+  //kmer_t * reverse_complements = kmers + num_records;
+  //add_reverse_complements(kmers, reverse_complements, num_records);
 
   // According to the paper linked below, merge sort is better for keys of 8 bytes
   // or more
