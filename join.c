cf70cb47941a9e79e7ff160adf5d6c20fd82a14f
alexbowe <bowe.alexander@gmail.com>
2014-06-12 15:37:49 +0900

    First attempt at join code.

--- join.c
+++ join.c
@@ -1,4 +1,5 @@
 #include "transform.h"
+#include "io.h"
 #include "join.h"
 
 // Remove rightmost nt (2 bits)
@@ -13,40 +14,59 @@
 // Haven't worked it out, but seems like id have to end up sorting it again, so probably faster to use two tables instead
 // especially since they are both a product of a single radix sort
 
-// left join
+// A - B
 size_t count_incoming_dummy_edges_64(uint64_t * table_a, uint64_t * table_b, size_t num_records, uint32_t k) {
+  #define get_a(i) (block_reverse_64(get_left_64(table_a[(i)])))
+  #define get_b(i) (block_reverse_64(get_right_64(table_b[(i)], k)))
   size_t count = 0;
   size_t a_idx = 0, b_idx = 0;
   uint64_t x = 0, y = 0;
+  char buf[k+1];
 
-  // Iterate over table a (first k-1 nts per record) as outer loop as x
-  for (;a_idx < num_records; a_idx++) {
-    // NOTE: comparison done colexically - need to reverse nts in integers
-    x = block_reverse_64(get_left_64(table_a[a_idx]));
-    fprintf(stderr, "x: %016llx\n", x);
-    // Iterate over table b (last k-1 nts per record) as inner loop as y
-    // Can safely skip non-matching values for y < x, as they correspond to outgoing dummy edges
-    for(;b_idx < num_records; b_idx++) {
-      y = block_reverse_64(get_right_64(table_b[b_idx], k));
-      fprintf(stderr, "y: %016llx\n", y);
-      // If x < y then count++ or output $x, and progress x
-      // Try to see how it works if we test x<=y instead (might be easier)
-      if (x < y) {
-        fprintf(stderr, "x < y\n");
-        count++;
-        break; // progress outer loop (x in table_a)
-      }
-      // else progress inner loop (y in table_b)
+  while (a_idx < num_records && b_idx < num_records) {
+    x = get_a(a_idx);
+    y = get_b(b_idx);
+
+    // TODO: handle duplicates in both tables
+    // These y-nodes from table B will require outgoing dummy edges
+    while (y < x) {
+      // add y to result
+      /*
+      uint64_t temp = get_right_64(table_b[b_idx],k) << 2;
+      sprint_kmer_acgt(buf, &temp, k);
+      buf[k-1] = '$';
+      fprintf(stderr, "%s\n", buf);
+      // TODO: add checks that we aren't going past the end of the arrays
+      */
+      y = get_b(++b_idx);
     }
-    // if y gets to end, then the rest of xs require dummy edges
-    // may need to check that they arent the same
-    // TODO: add checks for if this x is the same as the previous one
-    // (Same y check not needed since y only progresses if x >= y)
-    //if (b_idx >= num_records) {
-      //count += (num_records - a_idx);
-      //break;
+
+    // These x-nodes from table A will require incoming dummy edges
+    while (y > x) {
+      // add x to result
+      /*
+      uint64_t temp = table_a[a_idx] >> 2;
+      sprint_kmer_acgt(buf, &temp, k);
+      buf[0] = '$';
+      fprintf(stderr, "%s\n", buf);
+      */
+      x = get_a(++a_idx);
+    }
+
+    // These are the nodes that don't need dummy edges
+    // TODO: understand why these are printing when I have output a dummy - probably duplication?
+    while (y == x) {
+      // progress to end
+      while (y == x) {
+        uint64_t temp = table_a[a_idx];
+        sprint_kmer_acgt(buf, &temp, k);
+        fprintf(stderr, "%s\n", buf);
+        // if outputting, add x to result
+        x = get_a(++a_idx);
+      }
+      y = get_b(++b_idx);
+    }
   }
-  // if x gets to end, dont worry about it - all incoming dummy edges handled
   return count;
 }
 
