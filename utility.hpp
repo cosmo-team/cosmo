79866db76752b542dbda002c88a0b93db58a38d1
alexbowe <bowe.alexander@gmail.com>
2014-08-27 17:51:48 +0900

    Added binary search over function inputs.

---
+++ utility.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _UTILITY_HPP
+#define _UTILITY_HPP
+
+template <typename value_type, class IndexFunction>
+ssize_t function_binary_search(size_t lo, size_t hi, value_type key, IndexFunction f) {
+  while ( lo <= hi ) {
+    size_t mid = lo + (hi - lo)/2;
+
+    value_type temp = f(mid);
+    if (temp < key) lo = mid + 1;
+    else if (temp == key) return mid;
+    else if (mid == 0) return -1;
+    else hi = mid - 1;
+  }
+  return -1;
+}
+
+#endif
