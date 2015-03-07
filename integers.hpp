23dcc20d19bd998a89480e06cd25bd76e17ff888
alexbowe <bowe.alexander@gmail.com>
2015-03-08 06:05:48 +0900

    new integer implementation using ttmath

---
+++ integers.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef INTEGERS_HPP
+#define INTEGERS_HPP
+
+#include <ttmath/ttmath.h> // http://www.ttmath.org/
+
+namespace cosmo {
+
+// Have to define class so I can overload the istream >> operator
+// TODO: Add check for 32 bit OS
+template <size_t num_blocks>
+class big_uint : public ttmath::UInt<num_blocks> {};
+
+// Input operator used for reading from the file
+template <size_t num_blocks>
+std::istream& operator >> (std::istream& in, big_uint<num_blocks>& x) {
+  in.read((char*)&x, sizeof(big_uint<num_blocks>));
+  return in;
+}
+
+typedef big_uint<2> uint128_t;
+
+}
+
+#endif
